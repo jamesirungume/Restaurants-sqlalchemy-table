e42a4d240e1133cd22cd630c71bdc3adc9c007b5
Your Name <jymohcooler@gmail.com>
2023-09-03 12:38:10 +0300

    fixed errors

--- lib/test.py
+++ lib/test.py
@@ -13,12 +13,14 @@ if __name__ == '__main__':
     restaurant1 = Restaurant(name='Restaurant 1', price=3)
     restaurant2 = Restaurant(name='Restaurant 2', price=2)
 
-    customer1 = Customer(full_name='John Doe')
-    customer2 = Customer(full_name='Jane Smith')
+    customer1 = Customer(full_name='John Doe' , restaurants= [restaurant1, restaurant2])
+    customer2 = Customer(full_name='Jane Smith', restaurants = [restaurant1,restaurant2])
 
     # Add the data to the session and commit the changes
     session.add_all([restaurant1, restaurant2, customer1, customer2])
     session.commit()
+
+    
 
     review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
     review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer1)
@@ -37,6 +39,11 @@ if __name__ == '__main__':
     all_reviews = restaurant1.all_reviews()
     print('All reviews for Restaurant 1:')
     print(all_reviews)
+
+    reviewed_restaurants = customer1.restaurants
+    print(f"{customer1.full_name} has reviewed the following restaurants:")
+    for restaurant in reviewed_restaurants:
+        print(restaurant.name)
 
     # Example 3: Get the favorite restaurant for a customer
     favorite = customer1.favorite_restaurant()
