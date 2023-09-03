e5c87a011cbadd05c002a7e0229d3a4fb6ea5569
Your Name <jymohcooler@gmail.com>
2023-09-03 13:04:54 +0300

    fixed errors using ipdb

--- lib/seed.py
+++ lib/seed.py
@@ -20,8 +20,6 @@ if __name__ == '__main__':
     session.add_all([restaurant1, restaurant2, customer1, customer2])
     session.commit()
 
-    
-
     review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
     review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer1)
     review3 = Review(star_rating=3, restaurant=restaurant1, customer=customer2)
