ed3a830af96e73ce42890aeb5ee4151ce61c0bc4
Your Name <jymohcooler@gmail.com>
2023-09-03 13:16:51 +0300

    Finalizing the project

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# Restaurants-sqlalchemy-table
+Restaurant Review System using SQLAlchemy
+Table of Contents
+
+    Introduction
+    Project Overview
+    Getting Started
+    Project Structure
+    Usage Examples
+    Contributing
+    License
+
+Introduction
+
+This project demonstrates a simple restaurant review system built using SQLAlchemy, a powerful Object-Relational Mapping (ORM) library for Python. The system allows you to model restaurants, customers, and their reviews. You can perform various operations such as adding reviews, finding the fanciest restaurant, and more.
+Project Overview
+
+The project consists of the following components:
+
+    main.py: This is the main script where you can interact with the restaurant review system.
+    models.py: Contains SQLAlchemy model definitions for Restaurant, Customer, and Review.
+    restaurants.db: The SQLite database file where data is stored.
+
+Getting Started
+
+To get started with this project, follow these steps:
+
+    Clone the repository to your local machine:
+
+    bash
+
+git clone <repository-url>
+
+Install the required dependencies:
+
+bash
+
+pip install sqlalchemy
+
+Run the main.py script to interact with the restaurant review system:
+
+bash
+
+    python main.py
+
+Project Structure
+
+The project structure is as follows:
+
+diff
+
+- main.py
+- models.py
+- restaurants.db
+- README.md
+
+Usage Examples
+
+Here are some usage examples of the restaurant review system:
+
+    Adding a New Restaurant and Customer:
+
+    python
+
+restaurant1 = Restaurant(name='Restaurant 1', price=3)
+customer1 = Customer(full_name='John Doe')
+session.add_all([restaurant1, customer1])
+session.commit()
+
+Adding a Review:
+
+python
+
+review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
+session.add(review1)
+session.commit()
+
+Finding the Fanciest Restaurant:
+
+python
+
+fancy_restaurant = restaurant1.fanciest()
+print(f'The fanciest restaurant is: {fancy_restaurant.name}')
+
+Getting All Reviews for a Restaurant:
+
+python
+
+all_reviews = restaurant1.all_reviews()
+print('All reviews for Restaurant 1:')
+print(all_reviews)
+
+Finding a Customer's Favorite Restaurant:
+
+python
+
+favorite = customer1.favorite_restaurant()
+print(f"{customer1.full_name}'s favorite restaurant is: {favorite.name}")
+
+Deleting Reviews for a Restaurant by a Customer:
+
+python
+
+    customer1.delete_reviews(restaurant1)
+    session.commit()
+    print(f"Reviews for {customer1.full_name} at {restaurant1.name} have been deleted.")
+
+Contributing
+
+Contributions to this project are welcome! If you have any suggestions or improvements, please open an issue or submit a pull request.
+License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
--- lib/seed.py
+++ lib/seed.py
@@ -1,61 +1,45 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, Restaurant, Customer, Review  
+from models import Base, Restaurant, Customer, Review
+from faker import Faker
 
+# Initialize Faker
+fake = Faker()
 
+# Create the database engine and bind it to your SQLAlchemy models
 engine = create_engine('sqlite:///restaurants.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+def create_fake_restaurant():
+    return Restaurant(
+        name=fake.company(),
+        price=fake.random_int(min=1, max=5)
+    )
+
+def create_fake_customer():
+    return Customer(
+        full_name=fake.name()
+    )
+
+def create_fake_review(restaurant, customer):
+    return Review(
+        star_rating=fake.random_int(min=1, max=5),
+        restaurant=restaurant,
+        customer=customer
+    )
+
 if __name__ == '__main__':
-    # Create some sample data
-    restaurant1 = Restaurant(name='Restaurant 1', price=3)
-    restaurant2 = Restaurant(name='Restaurant 2', price=2)
+    # Generate fake data and add it to the database
+    for _ in range(10):  # Change the number to control the number of records
+        restaurant = create_fake_restaurant()
+        customer = create_fake_customer()
+        session.add_all([restaurant, customer])
+        session.commit()
 
-    customer1 = Customer(full_name='John Doe' , restaurants= [restaurant1, restaurant2])
-    customer2 = Customer(full_name='Jane Smith', restaurants = [restaurant1,restaurant2])
+        review = create_fake_review(restaurant, customer)
+        session.add(review)
+        session.commit()
 
-    # Add the data to the session and commit the changes
-    session.add_all([restaurant1, restaurant2, customer1, customer2])
-    session.commit()
-
-    review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
-    review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer1)
-    review3 = Review(star_rating=3, restaurant=restaurant1, customer=customer2)
-
-    # Add the reviews to the session and commit the changes
-    session.add_all([review1, review2, review3])
-    session.commit()
-
-    # Test various methods
-    # Example 1: Get the fanciest restaurant
-    fancy_restaurant = restaurant1.fanciest()
-    print(f'The fanciest restaurant is: {fancy_restaurant.name}')
-
-    # Example 2: Get all reviews for a restaurant
-    all_reviews = restaurant1.all_reviews()
-    print('All reviews for Restaurant 1:')
-    print(all_reviews)
-
-    reviewed_restaurants = customer1.restaurants
-    print(f"{customer1.full_name} has reviewed the following restaurants:")
-    for restaurant in reviewed_restaurants:
-        print(restaurant.name)
-
-    # Example 3: Get the favorite restaurant for a customer
-    favorite = customer1.favorite_restaurant()
-    print(f"{customer1.full_name}'s favorite restaurant is: {favorite.name}")
-
-    # Example 4: Add a new review for a restaurant
-    new_review = Review(star_rating=4, restaurant=restaurant2, customer=customer2)
-    session.add(new_review)
-    session.commit()
-    print("Added a new review")
-
-    # Example 5: Delete reviews for a restaurant by a customer
-    customer1.delete_reviews(restaurant1)
-    session.commit()
-    print(f"Reviews for {customer1.full_name} at {restaurant1.name} have been deleted.")
-
-    session.close()  # Close the session
+    session.close()
